dda0560d42516c5e24cac9737b496777a5475c18
Alex Gaynor <alex.gaynor@gmail.com>
2015-08-15 18:22:52 -0400

    rewrite stuff to use a parallelism abstraction

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,41 @@ use std::sync::{mpsc};
 use otp_cop::service::{CreateServiceResult, ServiceFactory};
 
 
+struct ParallelIter<T> {
+    count: usize,
+    pos: usize,
+    rx: mpsc::Receiver<T>
+}
+
+impl<T> Iterator for ParallelIter<T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<T> {
+        if self.pos < self.count {
+            return self.rx.recv().ok();
+        } else {
+            return None;
+        }
+    }
+}
+
+fn parallel<T, U, F1>(objs: Vec<T>, f1: F1) -> ParallelIter<U>
+        where F1: 'static + Fn(T) -> U + Send, T: Send, U: Send {
+    let (tx, rx) = mpsc::channel();
+    let mut threads = vec![];
+    for o in objs {
+        threads.push(thread::spawn(move || {
+            tx.send(f1(o));
+        }));
+    }
+
+    for t in threads {
+        t.join();
+    }
+
+    return ParallelIter{count: threads.len(), pos: 0, rx: rx};
+}
+
 fn main() {
     let service_factories = vec![
         Box::new(otp_cop::SlackServiceFactory) as Box<ServiceFactory>,
@@ -39,17 +74,7 @@ fn main() {
         print!("{}", opts.usage("otp-cop: <args>"));
     }
 
-    let (tx, rx) = mpsc::channel();
-    let count = services.len();
-    for service in services {
-        let tx = tx.clone();
-        thread::spawn(move || {
-            let result = service.get_users();
-            tx.send(result).unwrap();
-        });
-    }
-
-    for (i, result) in rx.iter().enumerate() {
+    for result in parallel(services, |service| service.get_users()) {
         println!("{}", result.service_name);
         println!("{}", "=".chars().cycle().take(result.service_name.len()).collect::<String>());
         println!("");
@@ -64,11 +89,7 @@ fn main() {
             };
             println!("@{}{}{}", user.name, email, details);
         }
-        if i + 1 != count {
-            println!("");
-            println!("");
-        } else {
-            break;
-        }
+        println!("");
+        println!("");
     }
 }
